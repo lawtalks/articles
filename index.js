04ef8588b3897146e5b5e28603fcef677e80621a
cold <jintiangao@sohu-inc.com>
2015-02-26 16:20:38 +0800

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var koa = require('koa');
-var app = module.exports = koa();
+var koa = require('koa.io');
 
-app.use(function *(){
-  this.body = 'Hello World';
+var app = koa();
+
+// middleware for koa
+app.use(function*() {
 });
 
-if (!module.parent) app.listen(process.env.PORT || 4000, function(){
-  console.log()
+
+// middleware for scoket.io's connect and disconnect
+app.io.use(function* (next) {
+  // on connect
+  yield* next;
+  // on disconnect
 });
+
+// router for socket event
+app.io.route('new message', function* () {
+  // we tell the client to execute 'new message'
+  var message = this.args[0];
+  this.broadcast.emit('new message', message);
+});
+
+
+if (!module.parent) app.listen(process.env.PORT || 4000);
